74861430e2efd6eb9677c4296b366d8132553e32
Konrad Kine <konrad.kine@tutanota.com>
2024-04-10 15:57:57 +0200

    feat: very simple setup with tmux only

---
+++ modules/nixos/user/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+with lib;
+with lib.minuszero; let
+  cfg = config.minuszero.user;
+in {
+  options.minuszero.user = {
+    enable = mkEnableOption "enable user module";
+
+    userName = mkOption {
+	default = "main-user";
+	description = ''Username'';
+    };
+
+    fullName = mkOption {
+	default = "Main User";
+	description = ''Full name'';
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+    users.users.${cfg.userName} = {
+      isNormalUser = true;
+      description = cfg.fullName;
+      initialPassword = "1234";
+      extraGroups = [ "networkmanager" "wheel" ];
+      shell = pkgs.fish;
+    };
+  };
+}
---
+++ systems/x86_64-linux/everest/default.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+with lib.minuszero; {
+  imports = [ ./hardware.nix ];
+
+  programs.fish.enable = true;
+
+  minuszero = {
+    cli-apps = {
+      tmux.enable = true;
+    };
+
+    user = {
+      enable = true;
+      userName = "surrphy";
+      fullName = "Konrad Kine";
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
