3d52a0f94c0a1a494d01918d2f8fa6c7c02740a9
Konrad Kine <konrad.kine@tutanota.com>
2024-04-21 11:03:53 +0200

    feat: podman

---
+++ modules/nixos/virtualisation/podman/default.nix
@@ -0,0 +1,28 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.minuszero;
+let cfg = config.minuszero.virtualisation.podman;
+in
+{
+  options.minuszero.virtualisation.podman = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Podman.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ podman-compose ];
+
+    # NixOS 22.05 moved NixOS Containers to a new state directory and the old
+    # directory is taken over by OCI Containers (eg. podman). For systems with
+    # system.stateVersion < 22.05, it is not possible to have both enabled.
+    # This option disables NixOS Containers, leaving OCI Containers available.
+    boot.enableContainers = false;
+
+    virtualisation = {
+      podman = {
+        enable = cfg.enable;
+        dockerCompat = true;
+      };
+    };
+  };
+}
